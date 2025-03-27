3ce9e9cb8981d59744651d1644965efee786a5c6
shuyulou <40105215+shuyulou@users.noreply.github.com>
2025-03-27 13:25:05 +0800

    Add files via upload

---
+++ train_rsicd.sh
@@ -0,0 +1,30 @@
+card=$1
+port=$2
+name=$3
+CUDA_VISIBLE_DEVICES=$card torchrun --nproc_per_node 4 --master_port $port -m training.main \
+    --train-data './dataset/train/ret2.csv' \
+    --images-dir './images' \
+    --csv-separator ',' --csv-img-key 'filename' --csv-caption-key 'title' \
+    --retrieval-data './dataset/test/rsicd_test.csv' \
+    --retrieval-images-dir './images' \
+    --retrieval-csv-separator ',' --retrieval-csv-img-key 'filename' --retrieval-csv-caption-key 'title' \
+    --retrieval-frequency 1 \
+    --epochs 20 --save-frequency 0 --batch-size 160 --workers 2 \
+    --lr 5e-6 --warmup 100 --weight_decay 0.5 --max-grad-norm 50 \
+    --image-model 'ViT-B-32' --image-model-builder 'openclip' \
+    --text-model 'ViT-B-32' --text-model-builder 'openclip' \
+    --pretrained-image-model --pretrained-text-model \
+    --loss 'InfoNCE' \
+    --report-to tensorboard --name $name \
+    --cache-dir './cache/weights' \
+
+CUDA_VISIBLE_DEVICES=$card torchrun --nproc_per_node 2   --master_port $port -m training.main \
+    --retrieval-data './dataset/test/rsicd_test.csv' --retrieval-images-dir './images' \
+    --retrieval-csv-separator ',' --retrieval-csv-img-key 'filename' --retrieval-csv-caption-key 'title' \
+    --retrieval-frequency 1 \
+    --save-frequency 0 --batch-size 128 --workers 2 \
+    --image-model 'ViT-B-32' --image-model-builder 'openclip' --text-model 'ViT-B-32' --text-model-builder 'openclip'\
+    --pretrained-image-model --pretrained-text-model \
+    --loss 'InfoNCE' \
+    --resume logs/$name/checkpoints/best.pt \
+    --cache-dir './cache/weights'
